96c85509dc23616515e7516fa48b7f8c34a600e1
SongWonJae <wjasong@inha.edu>
2017-04-24 01:18:20 +0900

    second

--- project.py
+++ project.py
@@ -24,22 +24,38 @@ def on_message(client, userdata, msg):
 	print(msg.payload)
 	value = str(msg.payload.decode("utf-8"))
 	if value  == '0':
-		GPIO.output(Motor1A,GPIO.HIGH)
-		GPIO.output(Motor1B,GPIO.LOW)
-		GPIO.output(Motor1E,GPIO.HIGH)
-		print('this is turn on the motor')
+		if confirm_M == 0 :
+			confirm_M = 1
+ 			GPIO.output(Motor1A,GPIO.HIGH)
+			GPIO.output(Motor1B,GPIO.LOW)
+			GPIO.output(Motor1E,GPIO.HIGH)
+			print('Action 1 detected')
+			print('turn on the motor')
+		elif confirm_M == 1 :
+			confirm_M = 0
+			GPIO.output(Motor1E,GPIO.LOW)
+			print('Action 1 detected again')
+			print('turn off the motor')
 	elif value == '1':
-		GPIO.output(Motor1E,GPIO.LOW)
-		print('this is turn off the motor')
+		if confirm_L == 0 :
+			confirm_L = 1
+			GPIO.output(LED1A, GPIO.HIGH)
+			print('Action 2 detected')
+			print('turn on the Light')
+		elif confirm_L == 1 :
+			confirm_L = 0
+			GPIO.output(LED1A, GPIO.LOW_
+			print('Action 2 detected agin')
+			print('turn off the Light')
 	elif value == '2':
-		GPIO.output(LED1A,GPIO.HIGH)
-		print('this is turn on the LED')
+		
+		print('Action 3 detected')
 	elif value == '3':
-		GPIO.output(LED1A,GPIO.LOW)
-		print('this is turn off the LED')
+
+		print('Action 4 detected')
 	else:
-		GPIO.output(Motor1E,GPIO.LOW)
-		print('this is else')
+		print('undefined Action detected.')
+		print('Do nothing')
 
 client = mqtt.Client()
 client.on_connect = on_connect
