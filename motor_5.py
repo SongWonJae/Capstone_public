fc4e5a83ace88e199806cf21dfd06bcbfec22b2a
SongWonJae <wjasong@inha.edu>
2017-05-15 21:46:19 +0900

    Fan, Led1, Led2, Door

--- motor_5.py
+++ motor_5.py
@@ -1,90 +1,90 @@
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
 GPIO.setmode(GPIO.BOARD)
-door_speed = 50
 
-class Door :
-	def __init__(self, doorOpen, doorClose, pinControl):
-
-		self.doorOpen = doorOpen
-		self.doorClose = doorClose
-		self.pinControl = pinControl
-		GPIO.setup(self.doorOpen,GPIO.OUT)
-		GPIO.setup(self.doorClose,GPIO.OUT)
-		GPIO.setup(self.pinControl,GPIO.OUT)
-		self.pwm_forward = GPIO.PWM(self.doorOpen, 100)
-		self.pwm_backward = GPIO.PWM(self.doorClose, 100)
-		self.pwm_forward.start(0)
-		self.pwm_backward.start(0)
-		GPIO.output(self.pinControl, GPIO.HIGH)
-
-	def forward(self,speed):
-
-		self.pwm_backward.ChangeDutyCycle(0)
-		self.pwm_forward.ChangeDutyCycle(speed)
-
-	def backward(self,speed):
-
-		self.pwm_forward.ChangeDutyCycle(0)
-		self.pwm_backward.ChangeDutyCycle(speed)
-
-	def stop(self):
-
-		self.pwm_forward.ChangeDutyCycle(0)
-		self.pwm_backward.ChangeDutyCycle(0)
-
-door = Door(16,18,22)
+Motor1A = 16
+Motor1B = 18
+Motor1E = 22
+Motor2E = 32
 LED1A = 26
+LED1B = 24
 
-confirm_M = 0
-confirm_L = 0
+confirm_M1 = 0
+confirm_M2 = 0
+confirm_L1 = 0
+confirm_L2 = 0
 
+GPIO.setup(Motor1A,GPIO.OUT)
+GPIO.setup(Motor1B,GPIO.OUT)
+GPIO.setup(Motor1E,GPIO.OUT)
+GPIO.setup(Motor2E,GPIO.OUT)
 GPIO.setup(LED1A,GPIO.OUT)
+GPIO.setup(LED1B,GPIO.OUT)
+
+Door = GPIO.PWM(Motor2E, 50)
+Door.start(0)
 
 def on_connect(client, userdata, flags, rc):
 	print('connected with result code '+str(rc))
 	client.subscribe(in_topic)
 
 def on_message(client, userdata, msg):
-	global confirm_M
-	global confirm_L
+	global confirm_M1
+	global confirm_M2
+	global confirm_L1
+	global confirm_L2
 
-	#print(msg.payload)
+	print(msg.payload)
 	value = str(msg.payload.decode("utf-8"))
-
 	if value  == '0':
-		if confirm_M == 0:
-			confirm_M = 1
-			door.forward(door_speed)
-			print('Action 1 detected')
-			print('turn on the motor\n')
-		elif confirm_M == 1:
-			confirm_M = 0
-			door.backward(door_speed)
-			print('Action 1 detected again')
-			print('turn off the motor\n')
-	elif value == '1':
-		if confirm_L == 0:
-			confirm_L = 1
+		if confirm_L1 == 0:
+			confirm_L1 = 1
 			GPIO.output(LED1A, GPIO.HIGH)
-			print('Action 2 detected')
-			print('turn on the Light\n')
-		elif confirm_L == 1:
-			confirm_L = 0
+			print('Action 1 detected')
+			print('turn on the Light\n')			
+		elif confirm_L1 == 1:
+			confirm_L1 = 0
 			GPIO.output(LED1A, GPIO.LOW)
-			print('Action 2 detected again')
+			print('Action1 detected again')
 			print('turn off the Light\n')
+	elif value == '1':
+		if confirm_M1 == 0:
+			confirm_M1 = 1
+			GPIO.output(Motor1A,GPIO.HIGH)
+			GPIO.output(Motor1B,GPIO.LOW)
+			GPIO.output(Motor1E,GPIO.HIGH)
+			print('Action2 detected')
+			print('turn on the Fan\n')
+		elif confirm_M1 == 1:
+			confirm_M1 = 0
+			GPIO.output(Motor1E,GPIO.LOW)
+			print('Action2 detected again')
+			print('turn off the Fan\n')
 	elif value == '2':
-		
-		print('Action 3 detected\n')
+		if confirm_M2 == 0:
+			confirm_M2 = 1
+			Door.ChangeDutyCycle(12)
+			print('Action3 detected')
+			print('open the Door')
+		elif confirm_M2 == 1:
+			confirm_M2 = 0
+			Door.ChangeDutyCycle(2.5)
+			print('Action3 detected again')
+			print('close the Door')
 	elif value == '3':
-
-		print('Action 4 detected\n')
-	else:
-		print('undefined Action detected.')
-		print('Do nothing\n')
+		if confirm_L2 == 0:
+			confirm_L2 = 1
+			GPIO.output(LED1B, GPIO.HIGH)
+			print('Action 4 detected')
+		elif confirm_L2 == 1:
+			confirm_L2 = 0
+			GPIO.output(LED1B, GPIO.LOW)
+			print('Action 4 detected again')
+	else :
+			print('Action detecting failed')
+			print('Please do again')
 
 client = mqtt.Client()
 client.on_connect = on_connect
